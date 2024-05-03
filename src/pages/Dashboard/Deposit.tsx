849d578018e25993eeeae41285b6b500518840af
khameleonstorm <khameleonstorm@gmail.com>
2024-05-03 22:16:39 +0100

    bal

--- src/pages/Dashboard/Deposit.tsx
+++ src/pages/Dashboard/Deposit.tsx
@@ -128,7 +128,7 @@ export default function Deposit() {
             <div className="flex-auto">
               <label htmlFor="coin" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select Method</label>
               <select onChange={handleCoinChange} id="coin" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 capitalize">
-                {coins.map((c:Coin, i:number) => <option key={i} value={JSON.stringify(c)}>{c.name} {c.network}</option>)}
+                {coins.map((c:Coin, i:number) => <option key={i} value={JSON.stringify(c)}>{c.name} ({c.network})</option>)}
               </select>
             </div>
 
