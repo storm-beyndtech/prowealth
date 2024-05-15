3f2773cce4030b99b9e24d73db430dbc11719191
khameleonstorm <khameleonstorm@gmail.com>
2024-05-15 12:09:43 +0100

    pdf

--- src/components/AdminSidebar.tsx
+++ src/components/AdminSidebar.tsx
@@ -56,7 +56,6 @@ const AdminSidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
       className={`text-sm absolute left-0 top-0 z-9999 flex h-screen w-72.5 flex-col overflow-y-hidden bg-black duration-300 ease-linear dark:bg-boxdark lg:static lg:translate-x-0 ${
         sidebarOpen ? 'translate-x-0' : '-translate-x-full'
       }`}
-      onClick={() => setSidebarOpen(false)}
     >
       {/* <!-- SIDEBAR HEADER --> */}
       <div className="flex items-center justify-between gap-2 px-6 py-5.5 lg:py-6.5">
